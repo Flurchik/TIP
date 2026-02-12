937c64d5935df6d4d481b9c077d21c922bc677ad
Flurchik <kirillsobachkin@gmail.com>
2026-02-12 15:29:28 +0300

    Update Task_2_12.02.2026.cpp

--- Task_2_12.02.2026.cpp
+++ Task_2_12.02.2026.cpp
@@ -2,30 +2,31 @@
 #include <string>
 using namespace std;
 
-string intToRoman(int num) {
-    //Массивы с данными, где каждый элемент i соответствует своему числу в своей системе исчисления
-    int value[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 }; 
-    string symbol[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-
-    string romanNum = "";
-
-    for (int i = 0; i < 13; i++) {
-        while (num >= value[i])  {
-            romanNum += symbol[i];
-            num -= value[i];
-        }
-    }
-    return romanNum;
+string intToRoman(int X) {
+    //Рекурсивное решение с помощью условного оператора if
+    if (X >= 1000) return "M" + intToRoman(X - 1000);
+    if (X >= 900)  return "CM" + intToRoman(X - 900);
+    if (X >= 500)  return "D" + intToRoman(X - 500);
+    if (X >= 400)  return "CD" + intToRoman(X - 400);
+    if (X >= 100)  return "C" + intToRoman(X - 100);
+    if (X >= 90)   return "XC" + intToRoman(X - 90);
+    if (X >= 50)   return "L" + intToRoman(X - 50);
+    if (X >= 40)   return "XL" + intToRoman(X - 40);
+    if (X >= 10)   return "X" + intToRoman(X - 10);
+    if (X >= 9)    return "IX" + intToRoman(X - 9);
+    if (X >= 5)    return "V" + intToRoman(X - 5);
+    if (X >= 4)    return "IV" + intToRoman(X - 4);
+    if (X >= 1)    return "I" + intToRoman(X - 1);
+    return "";
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
-
     int X;
+
     cout << "Введите число: ";
     cin >> X;
 
-    cout << "Результат: " << intToRoman(X) << endl;
-
+    cout << "Римское число: " << intToRoman(X) << endl;
     return 0;
 }
